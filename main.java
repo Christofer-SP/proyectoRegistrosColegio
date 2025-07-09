49b19d7f6d47bf677582814399bd51f8163587ae
JACS01 <jes34ileini@gmail.com>
2025-07-09 05:23:30 -0500

    Subiendo la clase de ejecución principal

---
+++ main.java
@@ -0,0 +1,13 @@
+package app;
+
+import Controlador.ControladorLogin;
+import ModeloRegistro.Usuario;
+
+import java.util.Scanner;
+import VistaRegistro.Login;
+
+public class main {
+    public static void main(String[] args) {
+        new Login().setVisible(true);
+    }
+}
